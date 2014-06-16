b341d1695ee0fae4c7d960fc21dae048cd563e79
vitaliy.vasilenko <vitaliy.vasilenko@engagepoint.com>
2014-06-16 11:29:46 +0300

    Done ArrayList, Queue ana Stack

---
+++ src/main/java/done/tasks/done/done/MyArrayList.java
@@ -0,0 +1,63 @@
+package done.tasks.done.done;
+
+public class MyArrayList <E> extends AbstractList<E> {
+
+    private E[] elements;
+
+    public MyArrayList() {
+        elements = (E[]) (new Object[0]);
+    }
+
+    @Override
+    public void add(E value) {
+        increaseCapacity(size + 1);
+        elements[size] = value;
+        size++;
+    }
+
+    @Override
+    public E set(int index, E value) {
+        rangeCheck(index);
+        E oldValue = elements[index];
+        elements[index] = value;
+        return oldValue;
+    }
+
+    @Override
+    public E get(int index) {
+        return elements[index];
+    }
+
+    @Override
+    public E remove(int index) {
+        rangeCheck(index);
+        E oldValue = elements[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elements, index+1, elements, index,
+                    numMoved);
+        elements[--size] = null;
+        return oldValue;
+    }
+
+    private void increaseCapacity(int capacity) {
+        if (checkRangeForIncrease(capacity)) return;
+        E[] newElements = (E[]) new Object[2 * capacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+    }
+
+    private boolean checkRangeForIncrease(int capacity) {
+        if (elements.length >= capacity) {
+            return true;
+        }
+        return false;
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size)
+            throw new IndexOutOfBoundsException();
+    }
+}
